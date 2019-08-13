5fb6a9dec8deea592901bd0c9b9b812da9f66829
Dan Kagan <dankagan7@gmail.com>
2019-08-13 12:43:25 -0400

    completed Oath Login for seeded users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    
     token = request.headers["Authentication"].split(" ")[1]
     payload = decode(token)
     user = User.find(payload["user_id"])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # get 'users/show'
   # get 'games/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/login', to: 'auth#create'
+  post '/login', to: 'auth#create' #needs username and password
+  get '/home', to: 'users#show' #take in the token
 
   resources :users, only: [:create]
   get '/users', to: 'users#index'
