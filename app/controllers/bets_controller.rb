19d228e46d5ed0549693ba7caff6d018d5685e19
Dan Kagan <dankagan7@gmail.com>
2019-08-19 19:36:06 -0400

    included game info in /Bet API, giving access to game info on MyBets

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -2,11 +2,11 @@ class BetsController < ApplicationController
 
     def index
         bets = Bet.all
-              render json: bets.to_json(
-              :except => [:created_at, :updated_at ]
-            )
+              render json: bets.to_json(:include => {
+                    :game => {:only => [:id, :game_date, :game_team1, :game_team2, :suggested_odds, :game_venue]}
+                },  :except => [:created_at, :updated_at ])
       end
-
+    
       def create
         render json: Bet.create(new_bet)
       end
@@ -14,7 +14,7 @@ class BetsController < ApplicationController
       private
 
       def new_bet
-        params.require(:bet).permit(:game_id, :user_id, :bet_taker_id, :money_line_odds, :wager_amount, :winner_id) 
+        params.require(:bet).permit(:user_id, :game_id, :bet_taker_id, :money_line_odds, :wager_amount, :winner_id) 
       end 
 
 
